5f94e9f192a6e16b36185b748c51c4e266c44753
Jakob Symchych <99896548+jakobsym@users.noreply.github.com>
2022-06-07 12:44:03 -0500

    Update assign4.c

--- assign4.c
+++ assign4.c
@@ -6,15 +6,17 @@
 
 void Age(unsigned short birth_y)
 {
-    //(birth_y < 9 || birth_y > 12)?(printf("Sorry. You are not between the age of 9 and 12.\n")):(printf("You are old enough to play! Welcome to the team!\n"));
-    if(birth_y < 9 || birth_y > 12)
+    unsigned short currYear = 2022;
+    unsigned short checkedYear;
+    checkedYear = (currYear - birth_y);
 
+    if(checkedYear < 9 || checkedYear > 12)
     {
-        printf("Sorry. You are not between the age of 9 and 12.\n");
+        printf("Sorry. You are not between the age of 9 and 12.\n\n");
     } 
     else
     {
-        printf("You are old enough to play! Welcome to the team!\n");
+        printf("You are old enough to play! Welcome to the team!\n\n");
 
     }
 
@@ -42,30 +44,41 @@ void City(char *city)
 
 
 int main() {
-    char first_n[32]; 
+    char first_n[32], choice; 
     unsigned short birth_y;
     char city[64];
     int i;
 
-    do
-    {
-        printf("What is the name of your city?\n");
-        scanf(" %[^\n]", city);
-        City(city);
-
-        printf("What is your first name?\n");
-        scanf("%s", first_n);
-        Name(first_n);
-
-        printf("How old are you?\n");
-        scanf("%hu", &birth_y);
-        Age(birth_y);
-        i++; //increment 'i' until we achieve 10 inputs
-    } while (i < 10);
-
-    if (i == 10){
-        printf("You have entered the maxmium number of people that can be on a team.");
-    }
     
+        do
+        {   
+            printf("Enter 'n' to quit, or 'c' to continue adding players.\n");
+            scanf(" %c", &choice);
+
+            if (choice == 'n'){
+                printf("Thanks for joining the team!\n");
+                break;
+
+            }
+            else if (choice == 'c'){
+                printf("What is the name of your city?\n");
+                scanf(" %[^\n]", city);
+                City(city);
+
+                printf("What is your first name?\n");
+                scanf("%s", first_n);
+                Name(first_n);
+
+                printf("What year were you born?\n");
+                scanf("%hu", &birth_y);
+                Age(birth_y);
+                i++;
+            }
+        } while (i < 10);
+
+        if (i == 10){
+            printf("You have entered the maxmium number of people that can be on a team.");
+        }
+
     return 0;
 }
